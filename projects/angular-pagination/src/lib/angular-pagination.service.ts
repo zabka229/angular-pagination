515f00db4b7b3161b706522cc52bafcda59acda5
Piotr <piotr.zaba@ready4s.com>
2021-06-08 15:13:13 +0200

    fix: use initial value

--- projects/angular-pagination/src/lib/angular-pagination.service.ts
+++ projects/angular-pagination/src/lib/angular-pagination.service.ts
@@ -1,25 +1,26 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { startWith, switchMap } from 'rxjs/operators';
 import { parseValue } from './utils';
 import { CallbackFunction } from './types';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AngularPaginationService {
-  constructor(private router: Router, private fb: FormBuilder) { }
+  constructor(private router: Router) { }
 
   public paginate(initialState: Record<string, any>, func: CallbackFunction): [FormGroup, Observable<any>] {
     const urlSearchParams = new URLSearchParams(window.location.search);
     const initFilters: Record<string, any> = {};
-    Object.keys(initialState).map((key: string) => initFilters[key] = parseValue(urlSearchParams.get(key)) || initialState[key]);
+    Object.keys(initialState).map((key: string) => initFilters[key] = new FormControl(parseValue(urlSearchParams.get(key)) || initialState[key]));
+    const filters: FormGroup = new FormGroup(initFilters);
 
-    const filters: FormGroup = this.fb.group(initFilters);
     const data = filters.valueChanges
       .pipe(
+        startWith(filters.value),
         switchMap((changedFilters: any) => {
           this.router.navigate([], { queryParams: changedFilters, replaceUrl: true });
           return func(changedFilters);
